67dbd285fe4846659af866720c612702ec6a0495
seralifatih <h.f.ilhan@gmail.com>
2026-03-30 13:23:09 +0300

    deployment

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,6 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import memoryQueue from "@opennextjs/cloudflare/overrides/queue/memory-queue";
 
-export default defineCloudflareConfig();
+export default defineCloudflareConfig({
+  queue: memoryQueue,
+});
